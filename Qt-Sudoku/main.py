1fd5927e4a6e7e05c1f68c90d528b6fa8c781afb
maryamsaeedi17 <m.saeedi64@gmail.com>
2023-03-14 01:25:18 +0330

    sudoku-check

--- Qt-Sudoku/main.py
+++ Qt-Sudoku/main.py
@@ -11,9 +11,34 @@ class MainWindow(QMainWindow):
         self.ui=Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.new_menu.triggered.connect(self.new_game)
+        self.ui.open_file_menu.triggered.connect(self.open_file)
         self.line_edits=[[None for i in range(9)] for j in range(9)]
         self.new_game()
-        
+
+    def open_file(self):
+        file_path=QFileDialog.getOpenFileName(self, "Open file...")[0]
+        f=open(file_path, "r")
+        big_text=f.read()
+        rows=big_text.split("\n")
+        puzzle_board=[[None for i in range(9)] for j in range(9)]
+        for i in range(len(rows)):
+            cells=rows[i].split(" ")
+            for j in range(len(cells)):
+                puzzle_board[i][j]=int(cells[j])
+
+        for i in range(9):
+            for j in range(9):
+                self.line_edits[i][j].setReadOnly(False)
+                if puzzle_board[i][j] != 0:
+                    self.line_edits[i][j].setText(str(puzzle_board[i][j]))
+                    self.line_edits[i][j].setReadOnly(True)
+                else:
+                    self.line_edits[i][j].setText("")
+
+
+
+
+
     def new_game(self):
         puzzle=Sudoku(3, seed=random.randint(1,1000)).difficulty(0.5)
         for i in range(9):
@@ -26,6 +51,39 @@ class MainWindow(QMainWindow):
                 new_cell.textChanged.connect(partial(self.validation, i, j))
                 self.line_edits[i][j] = new_cell
 
+    def check(self):
+        for i in range(9):
+            nums = []
+            for j in range(9):
+                num = self.line_edits[i][j].text()
+                if num != "":
+                    if num in nums:
+                        return False
+                    else:
+                        nums.append(num)
+
+        for i in range(9):
+            nums = []
+            for j in range(9):
+                num = self.line_edits[j][i].text()
+                if num != "":
+                    if num in nums:
+                        return False
+                    else:
+                        nums.append(num)
+
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                nums = []
+                for k in range(i, i+3):
+                    for l in range(j, j+3):
+                        num = self.line_edits[k][l].text()
+                        if num != "":
+                            if num in nums:
+                                return False
+                            else:
+                                nums.append(num)
+
 
 
 
@@ -33,7 +91,17 @@ class MainWindow(QMainWindow):
         if text not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
             self.line_edits[i][j].setText("")
 
-
+        if self.check() == False:
+            self.line_edits[i][j].setStyleSheet("color: red; background-color: pink;")
+        else:
+            self.line_edits[i][j].setStyleSheet("color: blue; background-color: rgb(157, 195, 230);")
+            for k in range(9):
+                for l in range(9):
+                    if self.line_edits[k][l] == "":
+                        break
+            else:
+                msg_box = QMessageBox()
+                msg_box.setText("You win!")
 
 
 
